3c2627ccabd7ca594c5abdee145075fdaaa6dde5
Markus <markus@haslinger.it>
2020-01-30 19:18:56 +0100

    Update Startup.cs
    disable https redirect

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace BulkInsertDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
